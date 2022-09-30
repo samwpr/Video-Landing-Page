b0dcb5e67efc1ced7a6f31950aa401b375a4143a
samwpr <swpr10x@gmail.com>
2022-09-30 16:18:24 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const btnEl = document.querySelector(".btn");
+const closeIconEl = document.querySelector(".close-icons");
+const trailerContainerEl = document.querySelector(".trailer-container");
+const videoEl = document.querySelector(".video");
+
+btnEl.addEventListener("click", () => {
+    trailerContainerEl.classList.remove("active");
+});
+
+closeIconEl.addEventListener("click", () => {
+    trailerContainerEl.classList.add("active");
+    videoEl.pause();
+});
